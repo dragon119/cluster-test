9fb3cd98188c98866887400932c90ff1fb9cd489
Masashi Narumoto <masashin@microsoft.com>
2023-12-19 11:08:46 -0800

    Update WeatherForecastMsgCOntroller.cs

--- testapp/Controllers/WeatherForecastMsgCOntroller.cs
+++ testapp/Controllers/WeatherForecastMsgCOntroller.cs
@@ -1,3 +1,6 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Confluent.Kafka;
 using System;
 using Microsoft.Extensions.Configuration;
@@ -13,7 +16,7 @@ namespace testapp.Controllers;
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastMsgController(ILogger<WeatherForecastController> logger)
-    
+        {
             _logger = logger;
         }
 
@@ -21,40 +24,19 @@ namespace testapp.Controllers;
         public async Task<string> Get()
         {
 
-             IConfiguration configuration = new ConfigurationBuilder()
-            .AddIniFile(args[0])
-            .Build();
+                const string topic = "purchases";
 
-        const string topic = "purchases";
+                var config = new ProducerConfig
+                {
+                        //BootstrapServers = Environment.GetEnvironmentVariable("KAFKA_BOOTSTRAP_SERVER"),
+                        BootstrapServers = "localhost:9092",
+                };
 
-        string[] users = { "eabara", "jsmith", "sgarcia", "jbernard", "htanaka", "awalther" };
-        string[] items = { "book", "alarm clock", "t-shirts", "gift card", "batteries" };
-
-        using (var producer = new ProducerBuilder<string, string>(
-            configuration.AsEnumerable()).Build())
-        {
-            var numProduced = 0;
-            Random rnd = new Random();
-            const int numMessages = 10;
-            for (int i = 0; i < numMessages; ++i)
-            {
-                var user = users[rnd.Next(users.Length)];
-                var item = items[rnd.Next(items.Length)];
-
-                producer.Produce(topic, new Message<string, string> { Key = user, Value = item },
-                    (deliveryReport) =>
-                    {
-                        if (deliveryReport.Error.Code != ErrorCode.NoError) {
-                            return ErrorCode.ToString();
-                        }
-                        else {
-                            Console.WriteLine($"Produced event to topic {topic}: key = {user,-10} value = {item}");
-                            numProduced += 1;
-                        }
-
-                    });
-            }
-
-            producer.Flush(TimeSpan.FromSeconds(10));
-        }   return "Successfully sent messages to purchases"
+                using (var producer = new ProducerBuilder<Null, string>(config).Build())
+                {
+                        var result = await producer.ProduceAsync("weather", new Message<Null, string> { Value="a message" });
+                }
+                //producer.Flush(TimeSpan.FromSeconds(10));
+                return "Successfully sent messages to weather topic";
         }
+}
